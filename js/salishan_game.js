6d3d5f7d8aca70647656cfa973cee674d5251ba0
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-11-21 01:07:00 -0800

    adding new font

--- js/salishan_game.js
+++ js/salishan_game.js
@@ -2,70 +2,73 @@ import { getPrefix } from './head.js'
 import keyboard from './keyboard.js'
 
 const letters = keyboard.sort(() => 0.5 - Math.random())
-
 const normalize = str => str.normalize('NFC')
 
 customElements.define(
 	getPrefix('game'),
 	class extends HTMLElement {
-		constructor() {
-			super()
-		}
-
 		#data = {
 			currentLetter: 0,
-			points: undefined,
-			skipped: undefined,
-			seconds: undefined,
-			input: undefined,
-			target: undefined,
-			targetDescription: undefined,
-			interval: undefined,
+			points: 0,
+			skipped: 0,
+			seconds: 0,
+			input: null,
+			target: null,
+			targetDescription: null,
+			interval: null,
 		}
 
 		#template = `
 	<salishan-navbar></salishan-navbar>
-
     <nn-caja padding="1rem">
       <main>
-		<h1>Match the letters</h1>
 
-        <section class="game">
+			<section class="home">
+				<h1>Glyph Arena</h1>
+			</section>
+
+			<section class="game">
+			<div class="content">
+				<h2>Match the glyphs</h2>
+				<p class="hint">(press <strong>esc</strong> to skip a letter)</p>
+			</div>
+
+			<div class="cards">
+
 			<div class="card input">
-				<input type="text" class="input" placeholder="-" />
+				<input type="text" class="input" placeholder="-" autofocus />
 				<p class="description"></p>
 			</div>
 			<div class="card target">
-				<p class="input">${letters[this.#data.currentLetter].label}</p>
-				<p class="description">${letters[this.#data.currentLetter].description}</p>
+				<p class="input">${letters[0].label}</p>
+				<p class="description">${letters[0].description}</p>
 			</div>
+
+			</div>
+
+			<div class="score">
+				<span>Matched:</span>	
+				<span class="points">0</span>
+				<span>Skipped:</span>	
+				<span class="skipped">0</span>
+				<span>Total:</span>	
+				<span class="total">${letters.length}</span>
+				<span>Seconds:</span>
+				<span class="time">0</span>
+			</div>
+
 		</section>
-		<div class="score">
-			<span>Matched:</span>	
-			<span class="points">
-				${this.#data.points || 0}
-			</span>
-
-			<span>Skipped:</span>	
-			<span class="skipped">
-				${this.#data.skipped || 0}
-			</span>
-
-			<span>Total:</span>	
-			<span class="total">
-				${keyboard.length}
-			</span>
-
-			<span>Seconds:</span>
-			<span class="time">
-				${this.#data.seconds || 0}
-			</span>
-		</div>
       </main>
     </nn-caja>
-    
     <salishan-footer></salishan-footer>
 `
+
+		#updateDOM() {
+			this.#data.pointsElement.innerHTML = this.#data.points
+			this.#data.skippedElement.innerHTML = this.#data.skipped
+			this.#data.target.innerHTML = letters[this.#data.currentLetter]?.label || ''
+			this.#data.targetDescription.innerHTML = letters[this.#data.currentLetter]?.description || ''
+		}
 
 		#matchInputandTarget(e) {
 			this.#startTimer()
@@ -73,17 +76,30 @@ customElements.define(
 			const hasMatch = normalize(e.target.value) === normalize(this.#data.target.innerHTML)
 
 			if (hasMatch) {
-				++this.#data.currentLetter
-
+				this.#data.points++
+				this.#data.currentLetter++
 				e.target.value = ''
+				this.#updateDOM()
 
-				this.#data.target.innerHTML = letters[this.#data.currentLetter]?.label || ''
-				this.#data.targetDescription.innerHTML = letters[this.#data.currentLetter]?.description || ''
+				if (this.#data.points + this.#data.skipped >= letters.length) this.#finishGame()
+			}
+		}
 
-				this.#data.points.innerHTML = ++this.#data.points.innerHTML
+		#finishGame() {
+			console.log('scored')
+			clearInterval(this.#data.interval)
+		}
 
-				if (this.#data.points.innerHTML >= letters.length) {
-					clearInterval(this.#data.interval)
+		#handleKeydown = e => {
+			this.#startTimer()
+
+			if (e.key === 'Escape') {
+				if (this.#data.points + this.#data.skipped >= letters.length) {
+					this.#finishGame()
+				} else {
+					this.#data.skipped++
+					this.#data.currentLetter++
+					this.#updateDOM()
 				}
 			}
 		}
@@ -91,22 +107,29 @@ customElements.define(
 		#startTimer() {
 			if (!this.#data.interval) {
 				this.#data.interval = setInterval(() => {
-					this.#data.seconds.innerHTML = ++this.#data.seconds.innerHTML
+					this.#data.seconds++
+					this.#data.secondsElement.innerHTML = this.#data.seconds
 				}, 1000)
 			}
 		}
 
-		async connectedCallback() {
+		connectedCallback() {
 			this.innerHTML = this.#template
 
 			this.#data.input = this.querySelector('.card.input input')
 			this.#data.target = this.querySelector('.card.target .input')
 			this.#data.targetDescription = this.querySelector('.card.target .description')
-			this.#data.seconds = this.querySelector('.score .time')
-			this.#data.points = this.querySelector('.score .points')
-			this.#data.skipped = this.querySelector('.score .skipped')
+			this.#data.secondsElement = this.querySelector('.score .time')
+			this.#data.pointsElement = this.querySelector('.score .points')
+			this.#data.skippedElement = this.querySelector('.score .skipped')
 
 			this.#data.input.addEventListener('input', this.#matchInputandTarget.bind(this))
+			document.addEventListener('keydown', this.#handleKeydown)
+		}
+
+		disconnectedCallback() {
+			document.removeEventListener('keydown', this.#handleKeydown)
+			clearInterval(this.#data.interval)
 		}
 	}
 )
