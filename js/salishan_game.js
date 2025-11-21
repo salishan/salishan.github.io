d4f726e155ddd276507d7fe92e23519299f6d2fc
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-11-21 07:38:22 -0800

    adding new sections structure

--- js/salishan_game.js
+++ js/salishan_game.js
@@ -20,13 +20,79 @@ customElements.define(
 
 		#template = `
 	<salishan-navbar></salishan-navbar>
-    <nn-caja padding="1rem">
+    <nn-caja padding="1rem" max-width="900px">
       <main>
-			<section class="home">
+
+			<section class="home flex-column" data-hidden="true">
 				<h1>Glyph Arena</h1>
+
+				<p>A fast-paced educational game where players match unique glyphs from Salishan and other Indigenous languages. Type the displayed glyph correctly to earn points, or press Escape to skip it. The game help players practice reading and typing complex characters while testing their knowledge and reflexes.</p>
+
+				<nn-fila class="flex-column menu">
+					<nn-btn color="#3fe383ff" id="goto-game">
+						Start Game
+					</nn-btn>
+					<nn-btn color="#3fe383ff" id="goto-score">
+						Check Scoreboard
+					</nn-btn>
+				</nn-fila>
 			</section>
 
-			<section class="game">
+			<section class="form flex-column" data-hidden="false">
+				<h2>Submit your score</h2>
+
+				<nn-fila class="flex-column menu">
+					<input id="name" type="text" placeholder="name" autocomplete="off" autocorrect="off" spellcheck="false" />
+					<nn-btn color="#3fe383ff" id="submit-score">
+						Submit
+					</nn-btn>
+				</nn-fila>
+			</section>
+
+			<section class="scoreboard flex-column" data-hidden="true">
+				<h2>Scoreboard</h2>
+
+				<div class="table" role="table">
+					<nn-fila break="sm" class="table-header" gap=".25rem" role="row">
+						<nn-pilar role="columnheader" size="100% / 4 - 0.25rem">
+							Name
+						</nn-pilar>
+						<nn-pilar role="columnheader" size="100% / 4 - 0.25rem">
+							Matched
+						</nn-pilar>
+						<nn-pilar role="columnheader" size="100% / 4 - 0.25rem">
+							Failed / Skipped
+						</nn-pilar>
+						<nn-pilar role="columnheader" size="100% / 4 - 0.25rem">
+							Time
+						</nn-pilar>
+					</nn-fila>
+					<div class="table-body" role="rowgroup">
+						<nn-fila break="sm" gap=".25rem" role="row">
+							<nn-pilar role="columnheader" size="100% / 4 - 0.25rem">
+								1
+							</nn-pilar>
+							<nn-pilar role="columnheader" size="100% / 4 - 0.25rem">
+								2
+							</nn-pilar>
+							<nn-pilar role="columnheader" size="100% / 4 - 0.25rem">
+								3
+							</nn-pilar>
+							<nn-pilar role="columnheader" size="100% / 4 - 0.25rem">
+								4
+							</nn-pilar>
+						</nn-fila>
+					</div>
+				</div>
+				
+				<div class="menu">
+					<nn-btn color="#3fe383ff" id="goto-home">
+						Go Home
+					</nn-btn>
+				</div>
+			</section>
+
+			<section class="game" data-hidden="true">
 				<div class="content">
 					<h2>Match the glyphs</h2>
 					<p class="hint">(press <strong>enter</strong> to submit a glyph or <strong>esc</strong> to skip it)</p>
@@ -112,6 +178,10 @@ customElements.define(
 				this.#data.interval = setInterval(() => {
 					this.#data.seconds++
 					this.#data.secondsElement.innerHTML = this.#data.seconds
+
+					if (this.#data.seconds >= 120) {
+						this.#finishGame()
+					}
 				}, 1000)
 			}
 		}
